c8f768df4340d3bb08a72c49f0ff1c32954dc5a5
Manh Tran <manhtran@lgcns.com>
2026-04-06 16:44:03 +0700

    ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,5 @@
-from rich.traceback import install
-install()
+# from rich.traceback import install
+# install()
 
 import asyncio
 import os
@@ -9,12 +9,14 @@ from typing import Any, Dict, List
 import certifi
 from dotenv import load_dotenv
 # from langchain_chroma import Chroma
-from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
+
 from langchain_pinecone import PineconeVectorStore
 from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from logger import (Colors, log_error, log_header, log_info, log_success,
                     log_warning)
 
@@ -26,16 +28,23 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    show_progress_bar=False,
-    chunk_size=50,
-    retry_min_seconds=10,
-)
+embeddings =  OllamaEmbeddings(
+                model=os.environ['OLLAMA_EMBEDDING_MODEL'],
+                base_url=os.environ['OLLAMA_BASE_URL'],
+            )
+# OpenAIEmbeddings(
+#     model="text-embedding-3-small",
+#     show_progress_bar=False,
+#     chunk_size=50,
+#     retry_min_seconds=10,
+# )
+
 # vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
 vectorstore = PineconeVectorStore(
     index_name=os.environ["INDEX_NAME"], embedding=embeddings
 )
+
+
 tavily_extract = TavilyExtract()
 tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
 tavily_crawl = TavilyCrawl()
@@ -45,7 +54,7 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
     """Process documents in batches asynchronously."""
     log_header("VECTOR STORAGE PHASE")
     log_info(
-        f"📚 VectorStore Indexing: Preparing to add {len(documents)} documents to vector store",
+        f"📚 VectorStore Indexing: Preparing to add {len(documents)} chunks to vector store",
         Colors.DARKCYAN,
     )
 
@@ -55,7 +64,7 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
     ]
 
     log_info(
-        f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} documents each"
+        f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} chunks each"
     )
 
     # Each task gets its own vectorstore instance to avoid shared HTTP session conflicts
@@ -66,7 +75,7 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
         try:
             await vs.aadd_documents(batch)
             log_success(
-                f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} documents)"
+                f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} records)"
             )
         except Exception as e:
             log_error(f"VectorStore Indexing: Failed to add batch {batch_num} - {e}")
@@ -74,8 +83,22 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
         return True
 
     # Process all batches concurrently
+    
+    import time
+    
+    start = time.perf_counter()
+
     tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
     results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    end = time.perf_counter()
+    print(f"Elapsed time: {end - start:.2f} seconds")
+
+
+    # results = []
+    # for i, batch in enumerate(batches):
+    #     res = await add_batch(batch, i + 1)
+    #     results.append(res)
 
     # Count successful batches
     successful = sum(1 for result in results if result is True)
@@ -103,7 +126,7 @@ async def main():
         input={
             "url": "https://docs.langchain.com/oss/python/",
             "extract_depth": "advanced",
-            "max_depth": 5,
+            "max_depth": 1,
         }
     )
 
@@ -130,7 +153,7 @@ async def main():
     # Split documents into chunks
     log_header("DOCUMENT CHUNKING PHASE")
     log_info(
-        f"✂️  Text Splitter: Processing {len(all_docs)} documents with 4000 chunk size and 200 overlap",
+        f"✂️  Text Splitter: Processing {len(all_docs)} pages with 2000 chunk size and 100 overlap",
         Colors.YELLOW,
     )
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
